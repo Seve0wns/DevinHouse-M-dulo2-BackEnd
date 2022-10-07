7b8e48eeb2dbc57116dafece41dca5237e3aba9b
Seve0wns <jhonathanhartmann@alunos.utfpr.edu.br>
2022-10-07 18:09:24 -0300

    ex02 S02

--- semana02/exercicios/ex01/index.js
+++ semana02/exercicios/ex01/index.js
@@ -1,7 +1,23 @@
 import express from "express";
 
 const app=express();
+const PORT=3333;
+app.use(express.json())
 
-app.listen(3333,()=>{
+const pizzas=[];
+
+app.get("/pizzas",(req,res)=>{
+    const name=req.query.name?.toLowerCase()||"";
+    res.json(
+        pizzas.filter(pizza=>pizza.name.toLowerCase().include(name))
+    );
+});
+
+app.post("/pizzas",(req,res)=>{
+    pizzas.push(req.body);
+    res.status(201).json(pizzas.at(pizzas.length-1));
+});
+
+app.listen(PORT,()=>{
     console.log("Online");
 })
