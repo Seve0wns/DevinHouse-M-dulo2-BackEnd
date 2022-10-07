d86485ec4790c9173ea01df1711095b3d05ef933
Seve0wns <jhonathanhartmann@alunos.utfpr.edu.br>
2022-10-07 18:24:15 -0300

    ex03 S02

--- semana02/exercicios/ex01/index.js
+++ semana02/exercicios/ex01/index.js
@@ -5,6 +5,7 @@ const PORT=3333;
 app.use(express.json())
 
 const pizzas=[];
+const orders=[];
 
 app.get("/pizzas",(req,res)=>{
     const name=req.query.name?.toLowerCase()||"";
@@ -12,11 +13,28 @@ app.get("/pizzas",(req,res)=>{
         pizzas.filter(pizza=>pizza.name.toLowerCase().include(name))
     );
 });
-
 app.post("/pizzas",(req,res)=>{
-    pizzas.push(req.body);
-    res.status(201).json(pizzas.at(pizzas.length-1));
+    const {name,description,price,ingredients}=req.body;
+    const newPizza={name,description,price,ingredients}
+    if(name&&description&&price&&ingredients.length){
+        pizzas.push(newPizza);
+        return res.status(201).json(pizzas.at(pizzas.length-1));
+    }
+    res.status(400).json({message:"Um ou mais campos inválidos"});
 });
+
+app.get("/solicitations",(req,res)=>{
+    res.send(orders);
+});
+app.post("/solicitations",(req,res)=>{
+    const {name,cpf,address,phone,payment,obs,order}=req.body;
+    const newOrder={name,cpf,address,phone,payment,obs:obs||"",order};
+    if(name&&cpf&&address&&phone&&payment&&order.length){
+        orders.push(newOrder);
+        return res.status(201).json(orders.at(pizzas.length-1));
+    }
+    res.json({message:"Um ou mais campos inválidos"});
+})
 
 app.listen(PORT,()=>{
     console.log("Online");
