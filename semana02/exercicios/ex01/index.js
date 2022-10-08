7413bd09aef918ef1b8e43b8ecd98f40f1bdbcd1
Seve0wns <jhonathanhartmann@alunos.utfpr.edu.br>
2022-10-08 15:58:21 -0300

    ex06 S02

--- semana02/exercicios/ex01/index.js
+++ semana02/exercicios/ex01/index.js
@@ -1,22 +1,24 @@
 import express from "express";
 import uuid4 from "uuid4";
+import cors from "cors";
 
 const app=express();
 const PORT=3333;
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const pizzas=[];
-const orders=[];
+let orders=[];
 
 app.get("/pizzas",(req,res)=>{
     const name=req.query.name?.toLowerCase()||"";
     res.json(
-        pizzas.filter(pizza=>pizza.name.toLowerCase().include(name))
+        pizzas.filter(pizza=>pizza.name.toLowerCase().includes(name))
     );
 });
 app.post("/pizzas",(req,res)=>{
     const {name,description,price,ingredients}=req.body;
-    const newPizza={name,description,price,ingredients}
+    const newPizza={id:uuid4(),name,description,price,ingredients}
     if(name&&description&&price&&ingredients.length){
         pizzas.push(newPizza);
         return res.status(201).json(pizzas.at(pizzas.length-1));
@@ -25,20 +27,36 @@ app.post("/pizzas",(req,res)=>{
 });
 
 app.get("/solicitations",(req,res)=>{
-    res.json(orders);
+    const status=req.query.status?.toLowerCase()||"";
+    res.json(orders.filter(order=>order.status.toLowerCase().includes(status)));
 });
-app.get("/solicitations/:id",(req,res)=>{
+app.get("/solicitation/:id",(req,res)=>{
     const id=req.params.id||"";
     res.json(orders.filter(order=>order.id===id));
 });
 app.post("/solicitations",(req,res)=>{
     const {name,cpf,address,phone,payment,obs,order}=req.body;
-    const newOrder={id:uuid4(),name,cpf,address,phone,payment,obs:obs||"",order};
+    const newOrder={id:uuid4(),name,cpf,address,phone,payment,obs:obs||"",order,status:"in production"};
+    newOrder.order=order.map(pizzaID=>pizzas.find(pizza=>pizza.id===pizzaID));
     if(name&&cpf&&address&&phone&&payment&&order.length){
         orders.push(newOrder);
-        return res.status(201).json(orders.at(pizzas.length-1));
+        return res.status(201).json(orders[orders.length-1]);
     }
     res.json({message:"Um ou mais campos inválidos"});
+});
+app.put("/solicitation/:id",(req,res)=>{
+    const id=req.params.id||"";
+    const newStatus=req.body.status;
+    if(!newStatus) return res.status(400).json({message:"Status inválido."});
+    const order=orders.find((order)=>order.id===id)
+    if(!order) return res.status(404).json({message:"Pedido não encontrado."});
+    order.status=newStatus;
+    res.json(order);
+});
+app.delete("/solicitation/:id",(req,res)=>{
+    const id=req.params.id||"";
+    orders=orders.filter(order=>order.id!==id);
+    res.json("Pedido cancelado!");
 })
 
 app.listen(PORT,()=>{
